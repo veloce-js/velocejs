600d71260f8a4b41a6538d126db1b708ca8527b4
Joel Chu <joelckchu@gmail.com>
2022-03-21 16:54:24 +0800

    move the ssr-vue folder into the monorepo

---
+++ packages/ssr-vue/README.md
@@ -0,0 +1,21 @@
+# Development NOT for publish
+
+> This folder should not get publish, it's only for development purpose only
+
+This is for developing our Vue (3) + uWebSocket.js framework Veloce
+
+# Folder structure
+
+- app - where your front end code
+- src - where your back end code
+
+---
+
+# Tips and tricks
+
+If you have [esrun]() installed globally, then just `esrun file.ts` to execute the typscript file without compiling it.
+
+Or you could `pnpm run ts -- file.ts` using the already installed `esbuild-register` module
+
+
+03-2022
---
+++ packages/ssr-vue/prod-server.js
@@ -0,0 +1,67 @@
+// @ts-check
+const fs = require('fs')
+const path = require('path')
+const uWS = require('uWebSockets.js')
+const mime = require('mime-types')
+
+const port = process.env.PORT || 9001
+// This is the production server using uWebSocket.js
+const resolve = (p) => path.resolve(__dirname, p)
+
+uWS.
+  App({})
+  // serve up the static files
+  .get('/assets/*', async (res, req) => {
+    const url = req.getUrl()
+    const assetDir = path.join(__dirname, 'dist', 'client')
+    const file = fs.readFileSync(path.join(assetDir, url))
+    if (file) {
+      const mimeType = mime.lookup(url) || 'application/octet-stream'
+      console.log(mimeType, url)
+      res.writeHeader('Content-Type', mimeType)
+      res.end(file)
+    } else {
+      // @TODO
+      res.writeStatus(404)
+    }
+
+  })
+  .get('/*', async (res, req) => {
+
+    // Here we modified it for our SSR app
+    const indexProd = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
+    const manifest = /* @ts-ignore */ require('./dist/client/ssr-manifest.json')
+    const url = req.getUrl()
+
+    // now we just need to figure out how to serve the static files
+    // and we really shouldn't serve static files here, it should be
+    // the job of web server
+
+    console.log(`request url`, url)
+
+    // @TODO we should have a map to tell which should be SSG (cached)
+    // then look up here or even better prefix with a path
+    // and move to another handler to make code cleaner 
+
+
+    let template, render
+    template = indexProd
+    render = require('./dist/server/entry-server.js').render
+
+
+    const [appHtml, preloadLinks] = await render(url, manifest)
+
+    const html = template
+      .replace(`<!--preload-links-->`, preloadLinks)
+      .replace(`<!--app-html-->`, appHtml)
+
+    res.end(html)
+
+
+  }).listen(port, (token) => {
+    if (token) {
+      console.log('Listening to port ' + port);
+    } else {
+      console.log('Failed to listen to port ' + port);
+    }
+  })
