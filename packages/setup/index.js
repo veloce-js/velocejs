ffc051d6e76118b9a56fb5cf811a54e1d86d7858
Joel Chu <joelckchu@gmail.com>
2022-03-21 08:48:34 +0800

    setting up the setup api

--- packages/setup/index.js
+++ packages/setup/index.js
@@ -1,16 +1,32 @@
 // entry
-const prompts = require('prompts')
+#!/usr/bin/env node
+// import path from "node:path";
+// import minimist from "minimist";
+// import { Plop, run } from "plop";
 
+const path = require('path')
+const minimist = require('minimist')
+const { Plop, run } = require('plop')
+// const { dirname, join } = require('path')
+// const { fileURLToPath } = require('url')
 
-const questions = [
-  {
-    type: 'text',
-    name: 'projectName',
-    message: 'What is your project name'
+const args = process.argv.slice(2)
+const argv = minimist(args)
+
+// import { dirname } from "node:path";
+// import { fileURLToPath } from "node:url";
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+Plop.prepare({
+  cwd: argv.cwd,
+  configPath: path.join(__dirname, 'plopfile.js'),
+  preload: argv.preload || [],
+  completion: argv.completion
+}, env => Plop.execute(env, (env) => {
+  const options = {
+    ...env,
+    dest: process.cwd()
   }
-]
 
-prompts(questions)
-  .then(response => {
-    console.log(response)
-  })
+  return run(options, undefined, true)
+}))
---
+++ packages/setup/plopfile.js
@@ -0,0 +1,6 @@
+// plop file for all the commands
+// scan folder and inject them using cjs
+
+module.exports = function(plop) {
+  
+}
