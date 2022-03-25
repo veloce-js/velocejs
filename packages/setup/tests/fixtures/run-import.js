d264aa2456792de147cb59d3487b4b232bd67ee7
Joel Chu <joelckchu@gmail.com>
2022-03-25 11:27:44 +0800

    the import return the same pattern

--- packages/setup/tests/fixtures/run-import.js
+++ packages/setup/tests/fixtures/run-import.js
@@ -5,13 +5,12 @@ import getDirname from '../../src/dirname.js'
 
 const __dirname = getDirname(import.meta.url)
 
-importPlopfile(join(__dirname, 'dummy'))
+importPlopfile(join(__dirname, '..', '..', 'templates'))
   .then(fns => {
 
     if (!fns) {
       return console.log(`Got nothing`, fns)
     }
-
 
 
     fns.forEach(fn => {
