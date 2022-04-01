96e8f00a6dc0741135790578001345bd59633b81
Joel Chu <joelckchu@gmail.com>
2022-04-01 17:55:38 +0800

    allow serve static to serve up multiple directories

--- packages/server/src/base/serve-static.ts
+++ packages/server/src/base/serve-static.ts
@@ -8,16 +8,22 @@ import { HttpResponse, HttpRequest } from 'uWebSockets.js'
 /**
  * serve static files from assetDir
  */
-export function serveStatic(assetDir: string) {
+export function serveStatic(assetDir: string | string[]) {
+
+  const dirs = Array.isArray(assetDir) ? assetDir : [assetDir]
 
   return function(res: HttpResponse, req: HttpRequest) {
     const url = req.getUrl()
-    const file = fs.readFileSync(path.join(assetDir, url))
-    if (file) {
+    const file = dirs
+      .filter(dir => fs.existsSync(path.join(dir, url)))
+      .map(dir => fs.readFileSync(path.join(dir, url)))
+      
+    if (file.length) {
       const mimeType = mime.lookup(url) || 'application/octet-stream'
       // console.log(mimeType, url)
       res.writeHeader('Content-Type', mimeType)
-      res.end(file)
+      // first come first serve
+      res.end(file[0])
     } else {
       // @TODO
       res.writeStatus('404')
