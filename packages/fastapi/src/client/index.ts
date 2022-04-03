f0de29ac32312046315bce520d5269c53c77e6be
Joel Chu <joelckchu@gmail.com>
2022-04-03 20:30:33 +0800

    Try to get swc to work

---
+++ packages/fastapi/src/client/index.ts
@@ -0,0 +1,31 @@
+// just test the thing
+import 'reflect-metadata'
+
+function logParamTypes(target : any, key : string) {
+  var types = Reflect.getMetadata("design:paramtypes", target, key);
+
+  console.log('types', types)
+
+  var s = types.map(a => a.name).join();
+  console.log(`${key} param types: ${s}`);
+
+}
+
+//
+
+class Foo {
+
+  @logParamTypes
+  someFunc(
+    params1: string,
+    params2: number,
+    params3?: number | boolean
+  ) {
+    console.log('do nothing', params1, params2, params3)
+  }
+}
+
+
+const bar = new Foo()
+
+bar.someFunc('a', 1)
--- packages/fastapi/src/server/decorators/validator.ts
+++ packages/fastapi/src/server/decorators/validator.ts
@@ -1,1 +1,9 @@
 // the new Validator decorator
+
+
+function Validator() {
+  
+  return (target: any, propertyName: string, descriptor: any) => {
+
+  }
+}
