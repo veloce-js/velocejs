abeb8ff86543d26adda79f45c6f6659731b3b3fc
Joel Chu <joelckchu@gmail.com>
2022-04-08 13:46:37 +0800

    init run on install and update all workspace

---
+++ packages/hooks/src/use-window-resize.ts
@@ -0,0 +1,26 @@
+/**
+ * This factory method will return several properties and methods
+ *
+ */
+export function useWindowResizeFactory(ref) {
+  const width = ref(window.innerWidth)
+  const height = ref(window.innerHeight)
+  const handleResize = (): void => {
+    width.value = window.innerWidth
+    height.value = window.innerHeight
+  }
+  const onMounted = (): void => {
+    window.addEventListener('resize', handleResize)
+  }
+  const onUnmounted = (): void => {
+    window.removeEventListener('resize', handleResize)
+  }
+
+  return {
+    width,
+    height,
+    handleResize,
+    onMounted,
+    onUnmounted
+  }
+}
