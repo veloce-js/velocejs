e96a8b9ae2aa8a649dd5afbd8bb5e322be8401b2
Joel Chu <joelckchu@gmail.com>
2022-04-09 13:48:19 +0800

    build writer

--- packages/server/dist/base/write-json.js
+++ packages/server/dist/base/write-json.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getCorkWriter = exports.writeJson = void 0;
+const tslib_1 = require("tslib");
 const constants_1 = require("../constants");
 const status_1 = require("../status");
+const debug_1 = tslib_1.__importDefault(require("debug"));
+const debugFn = (0, debug_1.default)('velocejs:server:write-json');
 // just write the header and encode the JSON to string
 const writeJson = (res, jsonObj) => {
     const writer = (0, exports.getCorkWriter)(res);
@@ -11,11 +14,15 @@ const writeJson = (res, jsonObj) => {
     });
 };
 exports.writeJson = writeJson;
-// break this out for re-use 
+// break this out for re-use
 const getCorkWriter = (res) => {
-    return (payload, headers) => {
+    return (payload, headers, status) => {
+        // this could create a bug - if they pass the wrong status code
+        // then we fill it with 200 OK by default, it's hard to check
+        const _status = status ? (0, status_1.lookupStatus)(status) : status_1.C200;
+        debugFn(`status: ${status}`);
         res.cork(() => {
-            res.writeStatus(status_1.C200);
+            res.writeStatus(_status);
             if (headers) {
                 for (const key in headers) {
                     res.writeHeader(key, headers[key]);
