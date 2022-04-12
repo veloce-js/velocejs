48bbfa8134c19666f1a2c8737b4ab6ee19572a52
Joel Chu <joelckchu@gmail.com>
2022-04-12 08:16:27 +0800

    use only the fix.js script from now on to copy the type.d.ts files

--- packages/server/fix.js
+++ packages/server/fix.js
@@ -1,19 +1,26 @@
 // the cp works fine but the path are wrong so we need to create a script to do it
+// cp ./src/types.d.ts ./index.d.ts && cp ./src/types.d.ts ./dist/types.d.ts && pnpm fix:index
 const fs = require('fs-extra')
 const { join } = require('path')
+
+const src = join(__dirname, 'src', 'type.d.ts')
 const file = join(__dirname, 'index.d.ts')
 
-fs.readFile(file, (err, txt) => {
-  if (err) {
-    console.error('ERROR:', err)
-    return
-  }
-  const output = txt.toString().replace(/\/base\//gi, '/src/base/')
-  fs.writeFile(file, output, (err) => {
-    if (err) {
-      console.error(`Error on write:`, err)
-      return
-    }
-    console.log('done')
+fs.copy(src, join(__dirname, 'dist', 'type.d.ts'))
+  .then(() => fs.copy(src, file))
+  .then(() => {
+    fs.readFile(file, (err, txt) => {
+      if (err) {
+        console.error('ERROR:', err)
+        return
+      }
+      const output = txt.toString().replace(/\/base\//gi, '/src/base/')
+      fs.writeFile(file, output, (err) => {
+        if (err) {
+          console.error(`Error on write:`, err)
+          return
+        }
+        console.log('done')
+      })
+    })
   })
-})
