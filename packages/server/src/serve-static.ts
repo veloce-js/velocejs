a4a9445313c607441f9c89a7712793f4cadd3905
Joel Chu <joelckchu@gmail.com>
2022-05-03 20:26:19 +0800

    Small update the server-static method

--- packages/server/src/serve-static.ts
+++ packages/server/src/serve-static.ts
@@ -6,6 +6,7 @@ import { HttpResponse, HttpRequest } from './types'
 import { DEFAULT_FILE, CONTENT_TYPE } from './base/constants'
 import { getWriter, write404 } from './writers'
 import { lookup } from './base/mime'
+import { isFunction } from '@jsonql/utils'
 // import { toArray } from '@jsonql/utils'
 // import { toArr } from '@velocejs/bodyparser/utils'
 import debug from 'debug'
@@ -14,13 +15,20 @@ const debugFn = debug('velocejs:server:serve-static')
 /**
  * serve static files from assetDir
  */
-export function serveStatic(assetDir: string | string[]) {
+export function serveStatic(
+  assetDir: string | string[],
+  onAbortedHandler?: () => void
+) {
   const dirs: Array<string> = Array.isArray(assetDir) ? assetDir : [assetDir]
 
   return function(res: HttpResponse, req: HttpRequest) {
     // we need to provide a onAbortedHandler here
     res.onAborted(() => {
-      debugFn(`Serve static aborted`)
+      if (onAbortedHandler && isFunction(onAbortedHandler)) {
+        onAbortedHandler() // @TODO should we pass the res ?
+      } else {
+        debugFn(`Serve static aborted`)
+      }
     })
     let url = req.getUrl()
     if (url === '/') {
