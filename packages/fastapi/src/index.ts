4207b0064bca0528fc94ab74fe26992734823f0c
Joel Chu <joelckchu@gmail.com>
2022-05-03 17:40:17 +0800

    Fixing the types problem

--- packages/fastapi/src/index.ts
+++ packages/fastapi/src/index.ts
@@ -1,14 +1,13 @@
 // fast api
 import 'reflect-metadata'
-// re-export 
+// re-export
 export * from './server/fast-api'
 export * from './server/rest/routes'
 export * from './server/rest/rest'
 export * from './server/rest/validator'
 // re-export some of the methods that is useful here
 export {
-  bodyParser,
   getWriter,
   getContentType,
   lookupStatus
-} from '@velocejs/server/src'
+} from '@velocejs/server'
