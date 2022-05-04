e770ddb41563e9104b274796da518b6b847a4792
Joel Chu <joelckchu@gmail.com>
2022-05-04 22:45:48 +0800

    Add a fail test and it still work correctly

---
+++ packages/server/tests/write-json-error.test.ts
@@ -0,0 +1,44 @@
+// reply with a different status
+import test from 'ava'
+import { createApp, shutdownServer, jsonWriter } from '../src'
+import Fetch from 'node-fetch'
+import { HttpResponse } from '../dist/types'
+
+const port = 9003
+const payload = {data: [1,2,3]}
+const reply = { OK: false }
+
+let listenSocket: any = null
+test.before(() => {
+  createApp()
+    .post('/*', (res: HttpResponse) => {
+      jsonWriter(res)(reply, 417)
+    })
+    .listen(port, (token: any) => {
+      listenSocket = token
+      if (!token) {
+        console.log(`Start up server failed on`, port)
+      }
+    })
+})
+// clean up otherwise ava throw error
+test.after(() => {
+  shutdownServer(listenSocket)
+})
+
+test(`Testing the writeJson method`, async (t) => {
+  t.plan(2)
+
+  return Fetch(`http://localhost:${port}`, {
+      method: 'post',
+      body: JSON.stringify(payload),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => {
+      t.is(res.status, 417)
+      return res.json()
+    })
+    .then(json => {
+      t.deepEqual(json, reply)
+    })
+})
