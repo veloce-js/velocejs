04b72acb9928e789b8f7a941e6c68cfefdace81b
Joel Chu <joelckchu@gmail.com>
2022-05-10 11:37:35 +0800

    all lowercase for config file

--- packages/config/tests/base.test.ts
+++ packages/config/tests/base.test.ts
@@ -1,4 +1,28 @@
 import test from 'ava'
+import { join } from 'node:path'
+import { VeloceConfig } from '../src'
 
+let config1: VeloceConfig
+let config2: VeloceConfig
 
-test.todo(`You need to write some test`)
+test.before(async () => {
+  config1 = new VeloceConfig()
+  // config2 = new VeloceConfig(join(__dirname, 'fixtures', 'jsonql.config.ts'))
+})
+
+test(`Should able to automatically discover the config file`, async (t) => {
+
+  const config = await config1.getConfig()
+
+  console.log(config)
+
+  t.truthy(config)
+})
+
+test.skip(`Should able to read the custom path config file`, async (t) => {
+  const config = await config2.getConfig()
+
+  console.log(config)
+
+  t.truthy(config)
+})
