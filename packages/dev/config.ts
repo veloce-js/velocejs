4dd4f5a562b96ab7738879ad20fc0bb04b7c171d
Joel Chu <joelckchu@gmail.com>
2022-05-10 15:53:45 +0800

    work as expected

--- packages/dev/config.ts
+++ packages/dev/config.ts
@@ -5,8 +5,18 @@ import { VeloceConfig } from '@velocejs/config'
 const config = new VeloceConfig()
 
 config.isReady
-.then(config => {
-  console.info('config', config)
+.then(_config => {
+  console.info('config', _config)
+
+  config.getConfig('contract')
+    .then(result => {
+      console.log('result', result)
+    })
+    .catch(noResult => {
+      console.log('noResult', noResult)
+    })
+
+
 })
 .catch(e => {
   console.info('e', e)
