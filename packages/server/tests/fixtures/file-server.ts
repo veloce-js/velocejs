d273e9f808a118516a100217673bd3e74a6be7cc
Joel Chu <joelckchu@gmail.com>
2022-05-13 19:57:13 +0800

    just stub the file server implmenetation and finish it later when we get to test it

--- packages/server/tests/fixtures/file-server.ts
+++ packages/server/tests/fixtures/file-server.ts
@@ -1,2 +1,26 @@
 // test server to serve up files
-// @TODO 
+// @TODO
+import * as fs from 'node:fs'
+import { HttpResponse, HttpRequest, us_socket } from '../../src/types'
+import { createApp } from '../../src/create-app'
+import { pipeStreamOverResponse, getFileSize } from '../../src/lib/files'
+const port = 0
+
+createApp()
+  .get('/*', (res: HttpResponse, req: HttpRequest) => {
+    
+
+    // @TODO get the file path
+    const fileName = ''
+
+    const totalSize = getFileSize(fileName)
+
+    // @TODO search for the files
+    const readStream = fs.createReadStream(fileName)
+    pipeStreamOverResponse(res, readStream, totalSize)
+  })
+  .listen(port, (token: us_socket) => {
+    if (token) {
+
+    }
+  })
