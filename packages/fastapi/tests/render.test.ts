5ccba7c9d4c122cab44af4d1fc4e97d13c6e2484
Joel Chu <joelckchu@gmail.com>
2022-05-14 16:31:11 +0800

    render test completed

--- packages/fastapi/tests/render.test.ts
+++ packages/fastapi/tests/render.test.ts
@@ -14,19 +14,35 @@ test.after(() => {
   api.stop()
 })
 
-test(`Testing the render text render`, async t => {
+test(`Testing the render text render with $text`, async t => {
   const res = await Fetch(`${url}/stuff/text`)
   const text = await res.text()
   t.truthy(text)
 })
 
-test('Test the get markdown content', async t => {
+test('Test the get markdown content with $text', async t => {
   const res = await Fetch(`${url}/stuff/markdown`)
   // const text = await res.text()
   t.is(res.headers.get('content-type'), 'text/markdown')
 })
 
-test(`Test the get html content`, async t => {
+test(`Test the $html content`, async t => {
   const res = await Fetch(`${url}/stuff/html`)
   t.is(res.headers.get('content-type'), 'text/html')
+})
+
+test(`Test the new $json method`, async t => {
+  const res = await Fetch(`${url}/posts-test`, {
+    method: 'post',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({
+      year: 2022,
+      month: 5,
+      day: 14
+    })
+  })
+
+  const json = await res.json()
+  t.deepEqual(json, {date: '2022-5-14', slug: 'NOTHING'})
+
 })
