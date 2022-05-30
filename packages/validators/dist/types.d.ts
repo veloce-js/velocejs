baf8d34b70cbe08b213348f4a57bbb6497425007
Joel Chu <joelckchu@gmail.com>
2022-05-31 07:43:25 +0800

    @velocejs/validators publish to 0.3.0

---
+++ packages/validators/dist/types.d.ts
@@ -0,0 +1,14 @@
+// specify your types here
+import {
+  JsonqlPropertyParamMap,
+  JsonqlObjectValidateInput,
+  JsonqlArrayValidateInput,
+} from '@jsonql/validator/index'
+
+export declare type VeloceAstMap = {
+  [propertyName: string]: Array<JsonqlPropertyParamMap>
+}
+
+// export declare type AddValidationRuleFn = (validationMap: JsonqlObjectValidateInput | JsonqlArrayValidateInput) => void
+
+export declare type ValidationRuleRecord = JsonqlObjectValidateInput | JsonqlArrayValidateInput
