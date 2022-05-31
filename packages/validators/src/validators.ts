6f6d9abb76b473456592d15ea977522c027727d5
Joel Chu <joelckchu@gmail.com>
2022-05-31 20:06:27 +0800

    The plugin should have stored the raw input instead of the transformed one

--- packages/validators/src/validators.ts
+++ packages/validators/src/validators.ts
@@ -6,9 +6,18 @@ import type {
   MixedValidationInput
 } from '@jsonql/validator/index'
 import {
- Validators as JsonqlValidators
+ Validators as JsonqlValidators,
+ VALIDATE_KEY,
+ VALIDATE_ASYNC_KEY,
+ PLUGIN_KEY,
+ PLUGIN_FN_KEY,
+ PATTERN_KEY,
+ RULES_KEY,
+ NAME_KEY,
+ PARAMS_KEY,
 } from '@jsonql/validators'
-
+import { outputFileSync } from 'fs-extra'
+const KEYS = [VALIDATE_KEY, VALIDATE_ASYNC_KEY, PLUGIN_FN_KEY]
 /**
   Here we take the parent methods and onlly deal with the
   generate files / contract
@@ -42,4 +51,32 @@ export class Validators extends JsonqlValidators {
     return o
   }
 
+  /**
+    We only want the function and generate name for it and replace
+    the function inside the json file (for the contract)
+    then we will import it again on the client, they could use the name
+    to call it again
+  */
+  public createScriptFile(filename?: string) {
+    const { plugins } = this.exportAll()
+    let file = ''
+    // for schema
+    /*
+    for (const propName in json.schema) {
+
+    }
+    */
+    // for plugins, we might only support deliver plugin and inline fucntion all treat as server only
+    plugins.forEach((plugin: any) => {
+      KEYS.forEach((key: string) => {
+        if (plugin[key]) {
+          file += `const ${plugin[NAME_KEY]} = ${plugin[key].toString()}\n`
+        }
+      })
+    })
+    if (!filename) {
+      return file
+    }
+    return outputFileSync(filename, file)
+  }
 }
--- packages/validators/tests/export.test.ts
+++ packages/validators/tests/export.test.ts
@@ -9,10 +9,6 @@ let val: Validators
 test.before(() => {
   const astMap = readJsonSync(join(__dirname, 'fixtures', 'multi-api-map.json'))
   val = new Validators(astMap)
-})
-
-test(`Should able to export the addValidationRules script`, t => {
-
   val.registerPlugin('MyCoolPlug', {
     main(val: number) {
       return val !== 42
@@ -30,12 +26,26 @@ test(`Should able to export the addValidationRules script`, t => {
       return (val / 2) > 1
     }
   })
+  val.addRules('archive', {
+    id: { plugin: 'MyCoolPlug'}
+  })
+  val.addRules('archive', {
+    id: { plugin: 'moreThan', num: 40}
+  })
+})
 
+test(`Should able to export the addValidationRules script`, t => {
 
   const schema = val.exportAll()
-
   // console.dir(schema, { depth: null })
-
   t.truthy(schema)
+})
 
+test(`Should able to output the function to a file`, t => {
+
+  const file = val.createScriptFile()
+
+  console.log(file)
+
+  t.truthy(file)
 })
