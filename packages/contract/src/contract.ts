10e7f088e065cb5cf9b48b13f9a4d3505f9c1304
Joel Chu <joelckchu@gmail.com>
2022-06-03 15:20:51 +0800

    setup the basic test a lot of types need to fix before proceed

---
+++ packages/contract/src/contract.ts
@@ -0,0 +1,36 @@
+// start your project here
+import type {
+  VeloceAstMap,
+  // MixedValidationInput,
+} from '@velocejs/validators/index'
+import {
+  Validators
+} from '@velocejs/validators'
+import {
+  JsonqlContractWriter
+} from '@jsonql/contract'
+
+
+export class Contract extends JsonqlContractWriter {
+
+  constructor(
+    astMap: VeloceAstMap,
+    private _validators: Validators
+  ) {
+    // @ts-ignore wait until we update the upstream types then remove this warning
+    super(astMap)
+  }
+
+
+  public generate() {
+    const { schema } = this._validators.export()
+  // console.dir( schema, { depth: null })
+
+    this.appendValidations( schema )
+    // at this point should be the final call
+    const contract = this.output()
+    
+    return contract
+  }
+
+}
--- packages/contract/tests/base.test.ts
+++ packages/contract/tests/base.test.ts
@@ -1,4 +1,18 @@
 import test from 'ava'
+import { routeForContract } from './fixtures/route-for-contract'
+import { Contract } from '../src'
+import { Validators } from '@velocejs/validators'
+
+let con: Contract
+let val: Validators
+
+test.before(()=> {
+
+  val = new Validators(routeForContract)
+  con = new Contract(routeForContract, val)
+  
+
+})
 
 
 test.todo(`You need to write some test`)
