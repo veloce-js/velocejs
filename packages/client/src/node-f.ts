b70bca1fc20513f2fb868fdab29b116ff71b041d
Joel Chu <joelckchu@gmail.com>
2022-06-09 20:43:58 +0800

    setting the types for fetch

--- packages/client/src/node-f.ts
+++ packages/client/src/node-f.ts
@@ -1,1 +1,12 @@
 // using node-fetch
+// as experiement interface keep the same as the fetch api for compatibility
+import fetch from 'node-fetch'
+
+
+
+// main
+export default function main(url: string, options?: any) {
+  const opts = {
+    headers: { 'Content-Type': 'application/json' }, // @TODO change to jsonql next
+  }
+}
