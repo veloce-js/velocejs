9cfd94a2d851983fc97aa4ec277dc81e029bbf20
Joel Chu <joelckchu@gmail.com>
2022-06-13 06:40:45 +0800

    regenerate the test file for testing

--- packages/client/tests/base.test.ts
+++ packages/client/tests/base.test.ts
@@ -8,14 +8,13 @@ import jsonStr from './fixtures/contract/public-contract'
 const jsonOut = join(__dirname, 'fixtures', 'contract', 'public-contract.json')
 
 
-test(`You need to write some test`, t => {
+test(`dummy test to generate the correct contract file`, t => {
 
   // const file = readFileSync(jsonStr)
 
-
   const jsonFile = JSON.stringify(jsonStr)
 
-  // writeJsonSync(jsonOut, JSON.parse(jsonFile))
+  writeJsonSync(jsonOut, JSON.parse(jsonFile))
 
   t.truthy(jsonStr)
 })
