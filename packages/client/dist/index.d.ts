8798fc7de0a6ca5d3c848a86b8ad58ba5e651764
Joel Chu <joelckchu@gmail.com>
2022-06-15 15:02:49 +0800

    start moving over to vitest

--- packages/client/dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { VeloceClient } from './client';
---
+++ packages/client/tests/node-client.spec.ts
@@ -0,0 +1,7 @@
+// using vitest
+import { it } from 'vitest'
+
+it('should work', (ctx) => {
+  // prints name of the test
+  console.log(ctx.meta.name)
+})
