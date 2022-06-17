f053ab111ca40d3b7173347c146c12a8e062eef4
Joel Chu <joelckchu@gmail.com>
2022-06-17 10:19:52 +0800

    Add the first test file but realize need to fix the server-io-core about the proxy thing first

---
+++ packages/client/tests/qunit/files/client-async.qunit.js
@@ -0,0 +1,6 @@
+// testing the async clien without providing the contract
+QUnit.test('AsyncClient should able to connect to server and grab the contract', function(assert) {
+
+  
+  assert.equal(1,1)
+})
